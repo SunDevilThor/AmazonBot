e43e0e159a977ba0b9d87dc57eddf92c682d683a
SunDevilThor <thorkeane@yahoo.com>
2021-08-11 17:24:32 -0700

    got bot to sign into account successfully

--- AmazonBot.py
+++ AmazonBot.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 
 import time
+import config
 
 url = ('http://amazon.com')
 
@@ -15,10 +16,35 @@ url = ('http://amazon.com')
 browser = webdriver.Chrome('/Users/thor/Downloads/chromedriver') # Optional argument, if not specified will search path.
 browser.get(url)
 
+# Sign into Amazon account
+print('Signing into Amazon account')
+sign_in = browser.find_element_by_id('nav-link-accountList')
+sign_in.click()
+
+browser.implicitly_wait(3)
+
+enter_email = browser.find_element_by_id('ap_email')
+enter_email.click()
+
+enter_email.send_keys(config.email)
+
+continue_button = browser.find_element_by_id('continue')
+continue_button.click()
+
+enter_pw = browser.find_element_by_id('ap_password')
+enter_pw.click()
+
+enter_pw.send_keys(config.pw)
+
+sign_in_submit = browser.find_element_by_id('signInSubmit')
+sign_in_submit.click()
+print('Signed into account successfully')
+
 search_item = 'chapstick'
 
 search_box = browser.find_element_by_id('twotabsearchtextbox')
 search_box.click()
+print('Searching for item...')
 
 search_box.send_keys(search_item)
 
@@ -31,14 +57,9 @@ chapstick = browser.find_element_by_link_text('Blistex Medicated Lip Balm, 0.15 
 chapstick.click()
 
 # One-time purchase selection
-try:
-    one_time_purchase = browser.find_element_by_link_text('One-time purchase:')
-    one_time_purchase.click()
-    print('Selected one-time purchase successfully')
-
-except Exception as error: 
-    print(error)
-
+one_time_purchase1 = browser.find_element_by_class_name ('a-declarative')
+one_time_purchase1.click()
+print('Selected one-time purchase successfully')
 
 try:
     browser.implicitly_wait(3)
@@ -47,28 +68,21 @@ try:
     print('Added item to cart')
 
 except Exception as error: 
+    print('ADD TO CART FAILED')
     print(error)
  
-try:
-    browser.implicitly_wait(3)
-    proceed_to_checkout = browser.find_element_by_id('hlb-ptc-btn-native')
-    proceed_to_checkout.click()
+try: 
+    default_address = browser.find_element_by_class_name('a-button-input')
+    default_address.click()
 
-except Exception as error: 
+except Exception as error:
+    print('FAILED to select default shipping address')
     print(error)
 
-enter_email = browser.find_element_by_id('ap_email')
-enter_email.click()
+try:
+    done_button = browser.find_element_by_class_name('glowDoneButton')
+    done_button.click()
 
-
-
-
-
-
-
-
-
-
-
-
-
+except Exception as error:
+    print('FAILED to click on yellow Done button')
+    print(error)
